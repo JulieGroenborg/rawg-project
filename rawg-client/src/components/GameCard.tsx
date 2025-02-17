09d41ec276d3aeac0b18ff288909c0899bed7e62
Julie <julieeriksen09@hotmail.com>
2025-02-17 10:48:24 +0100

    platforms icons

--- rawg-client/src/components/GameCard.tsx
+++ rawg-client/src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import PlatformIconsList from "./PlatformIconsList";
 
 interface Props {
   game: Game;
@@ -12,6 +13,10 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image}></Image>
       <CardBody>
         <Heading fontSize="2x1">{game.name}</Heading>
+        {/* {game.parent_platforms.map((parent_platform) => (
+          <Text key={parent_platform.platform.id}>{parent_platform.platform.name}</Text>
+        ))} */}
+        <PlatformIconsList platforms={game.parent_platforms.map((p) => p.platform)}></PlatformIconsList>
       </CardBody>
     </Card>
   );
